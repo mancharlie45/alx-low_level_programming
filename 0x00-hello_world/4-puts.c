46cee6c10bd33a90b0582fcfbaac45ba0e9553c9
mancharlie45 <ubahcharl45@gmail.com>
2022-09-08 07:53:13 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * Description: main - prints out a string to stdout
+ * return: 0 if success.
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzel");
+return(0);
+}
