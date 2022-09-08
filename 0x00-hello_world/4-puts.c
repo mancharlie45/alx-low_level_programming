6f6f6d852cc00025e6842a49810b3a203c800920
mancharlie45 <ubahcharl45@gmail.com>
2022-09-08 08:09:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * Description: main - prints out a string to stdout
  * return: 0 if success.
  */
-int main(void)
+int main (void)
 {
-
-   puts("\"Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
  return(0);
 }
