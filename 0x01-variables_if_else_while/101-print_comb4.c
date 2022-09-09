ebfce7414e122d07f6a9bf7af7bf73b74673a3c8
mancharlie45 <ubahcharl45@gmail.com>
2022-09-09 13:23:21 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,7 +25,7 @@ while (n3 <= 57)
 putchar(n1);
 putchar(n2);
 putchar(n3);
-if (n1 != 55 || n2 != 56 || n3 <= 57)
+if (n1 != 55 || n2 != 56 || n3 != 57)
 {
 putchar(com);
 putchar(32);
@@ -38,4 +38,4 @@ n1 += 1;
 }
 putchar('\n');
 return (0);
-}	
+}
