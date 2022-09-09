fbc678127935c1051c83f71808f0d74bea19fbf1
mancharlie45 <ubahcharl45@gmail.com>
2022-09-09 06:24:56 -0700

    tast 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <type.h>
+
+/**
+ * main - program that prints the alphabet in lower case, followed by a bew line
+ * You can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+int l = 'a';
+while (l <= 'z')
+{
+putchar(1);
+l += 1;
+}
+putchar('\n');
+return (0);
+}
