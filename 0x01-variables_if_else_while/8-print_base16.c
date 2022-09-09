417783d2ca1116decc37709ab1a7fdf532e0424f
mancharlie45 <ubahcharl45@gmail.com>
2022-09-09 11:04:45 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,8 @@ int main(void)
 int d;
 char low;
 for (d = '0'; d <= '9'; d++);
-for (low = 'a'; low <= 'f'; low++)
+putchar(d);
+for (low = 'a'; low <= 'f'; low++);
 putchar(low);
 putchar('\n');
 return (0);
