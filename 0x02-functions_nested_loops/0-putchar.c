90b0342e583cf1a52aac4fdd7dadfe3d5bcec52d
mancharlie45 <ubahcharl45@gmail.com>
2022-09-13 12:22:00 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: init
+ */
+
+int main(void)
+{
+int n = 0;
+char str_put[8] = "_putchar";
+
+while (n < 8)
+{
+_putchar(str_put[n]);
+n += 1;
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+scripts for functions nested loops
