fc4a75aa7ca8340eac3470470dd74fc9f8921e07
mancharlie45 <ubahcharl45@gmail.com>
2022-09-13 12:48:19 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 int n = 0;
-char str_put[7] = "putchar";
+char str_put[8] = "putchar";
 
-while (n < 7)
+while (n < 8)
 {
-_putchar(str_put[n]);
+putchar(str_put[n]);
 n += 1;
 }
-_putchar('\n');
+putchar('\n');
 return (0);
 }
