546264700cdc49c513febb9205528783e445021b
mancharlie45 <ubahcharl45@gmail.com>
2022-09-13 22:54:17 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+putchar('+');
 return (1);
 }
-else if (n < 0)
+else if (n == 0)
 {
-putchar(45);
-return (-1);
-}
-else (n == 0)
-{
-putchar(48);
+putchar('0');
 return (0);
+}
+else
+{
+putchar('-');
+return (-1);
 }
 }
