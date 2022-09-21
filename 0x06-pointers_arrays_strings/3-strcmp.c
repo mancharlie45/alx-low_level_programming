855f961e8d1f5218d99e2964dc7486665877f83f
mancharlie45 <ubahcharl45@gmail.com>
2022-09-21 14:53:53 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _strcmp - compares two string
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ *
+ */
 {
 int i;
 
