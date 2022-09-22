4d90a2d22d4e6bd04ec4918d2afceb9dea3d3b1f
mancharlie45 <ubahcharl45@gmail.com>
2022-09-21 23:19:04 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,7 @@ void print_number(int n)
 unsigned int m, d, count;
 if (n < 0)
 {
-_putchar(45);
+putchar(45);
 m = n * -1:
 }
 else
@@ -27,6 +27,6 @@ count *= 10;
 }
 for (; count >= 1; count /= 10)
 {
-_putchar(((m / count) % 10) + 48);
+putchar(((m / count) % 10) + 48);
 }
 }
