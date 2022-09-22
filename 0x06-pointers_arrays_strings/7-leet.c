f9dc4afb3d7eed010e5a7414f546d4295bbc109d
mancharlie45 <ubahcharl45@gmail.com>
2022-09-21 22:37:52 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,7 +16,7 @@ while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
 {
-if (*(s + count) == low_letter[i] || *(s + count) == upp_letters[i])
+if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 {
 *(s + count) = numbers[i];
 break;
