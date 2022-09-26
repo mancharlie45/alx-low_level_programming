d729503c4661ae0487dc3b8bbe28bd0bc90998f1
mancharlie45 <ubahcharl45@gmail.com>
2022-09-26 10:59:33 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int_strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char);
+void print_chessboard(char (*a)[18]);
+void print_diagsiums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
