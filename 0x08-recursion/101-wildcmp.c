4c27acecbf18f957bfea1a880e5ac9f7d0d27e88
mancharlie45 <ubahcharl45@gmail.com>
2022-09-28 12:43:34 -0700

    task 7

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+int check_wildcmp(char *s);
+
+/**
+ * wildcmp - Returns if a string is wildcmp
+ * @s: the string value to be checked
+ *
+ * Return: integer value
+ */
+int wildcmp(char *s)
+{
+if (*s == '0')
+return (1);
+
+return (check_wildcmp(s));
+}
+
+/**
+ * check_wildcmp - check if a string is wildcmp
+ * @s: the string value to be checked
+ * Return: integer value
+ */
+int check_wildcmp(char *s)
+{
+int l = _strlen_recursion(s) - 1;
+
+
+if (*s == s[l])
+{
+s++;
+l--
+}
+else
+{
+return (0);
+}
+
+return (1);
+}
+
+/**
+ * _strlen_recursion - Get the length of a string
+ * @s: the string to get the length
+ *
+ * Return: the string length
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+
+s++;
+return (_strlen_recursion(s) + 1);
+}
