b051461a08b76cdc2ef76e16b220771349862100
mancharlie45 <ubahcharl45@gmail.com>
2022-10-20 23:28:53 -0700

    task 2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,20 +15,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+char *dup;
+int len;
 list_t *new;
-size_t nchar;
 
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
 
-new->str = strdup(str);
-for (nchar = 0; str[nchar]; nchar++)
-;
+dup = strdup(str);
+if (dup == NULL)
+{
+free(new);
+return (NULL):
+}
 
-new->len = nchar;
+for (len = 0; str[len];)
+len++;
+
+new->str = dup;
+new->len = len;
 new->next = *head;
+
 *head = new;
 
-return (head);
+return (new);
 }
