8d4543abd8325734266c0fc5f87b9d0d630c7b52
mancharlie45 <ubahcharl45@gmail.com>
2022-10-31 15:19:14 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,7 +72,6 @@ exit(97);
 buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
-to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 do{
 if (from == -1 || r == -1)
@@ -81,6 +80,7 @@ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 w = write(to, buffer, r);
 if (to == -1 || w == -1)
 {
@@ -89,7 +89,6 @@ free(buffer);
 exit(99);
 }
 r = read(from, buffer, 1024);
-to = open(argv[2], O_WRONLY | O_APPEND);
 } while (r > 0);
 free(buffer);
 close_file(from);
